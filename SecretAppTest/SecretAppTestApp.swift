a87047de35d3a41f9af3d978a7e8855dcb97b49c
Roman.Pro <136555465+RomanPronskiy@users.noreply.github.com>
2025-08-16 13:05:38 +0300

    Initial Commit

---
+++ SecretAppTest/SecretAppTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecretAppTestApp.swift
+//  SecretAppTest
+//
+//  Created by Roman Pronskyi on 16/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct SecretAppTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
