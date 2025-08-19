1e21943206453acbb661afd314dd1394f364e61e
Roman.Pro <136555465+RomanPronskiy@users.noreply.github.com>
2025-08-19 11:12:15 +0300

    Added MyCode View Model

---
+++ Modules/Features/MyCode/UI/MyCodeViewModel.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+struct MyCodeViewModel {
+  let navigationTitle: String
+  let address: String
+  let name: String
+  let coinsImages: [Data]
+  let coinsLabel: String
+  let destinationNetwork: String
+  let QRImage: Data
+  let copyLabel: String
+  let isCopied: Bool
+}
