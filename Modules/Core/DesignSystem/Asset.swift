d7661b700d0bdf1b9ad1cb6d243c6fbd4e23e3f5
Roman.Pro <136555465+RomanPronskiy@users.noreply.github.com>
2025-08-19 11:11:32 +0300

    Updated Asset by extending it with UI Image

--- Modules/Core/DesignSystem/Asset.swift
+++ Modules/Core/DesignSystem/Asset.swift
@@ -15,7 +15,7 @@ public enum Asset {
 }
 
 extension Asset {
-  public var swiftUIImage: some View {
+  public var swiftUIImage: Image {
     switch self {
     case .checkMark: Image(.checkMark)
     case .copy: Image(.copy)
@@ -28,6 +28,22 @@ extension Asset {
     case .coins_v1: Image(.coinsV1)
     case .coins_v2: Image(.coinsV2)
     case .coins_v3: Image(.coinsV3)
+    }
+  }
+
+  public var uiImage: UIImage {
+    switch self {
+    case .checkMark: UIImage(resource: .checkMark)
+    case .copy: UIImage(resource: .copy)
+    case .cross: UIImage(resource: .cross)
+    case .eyeOn: UIImage(resource: .eyeOn)
+    case .flashLight: UIImage(resource: .flashLight)
+    case .logo: UIImage(resource: .logo)
+    case .qrAngle: UIImage(resource: .qrAngle)
+    case .share: UIImage(resource: .share)
+    case .coins_v1: UIImage(resource: .coinsV1)
+    case .coins_v2: UIImage(resource: .coinsV2)
+    case .coins_v3: UIImage(resource: .coinsV3)
     }
   }
 }
