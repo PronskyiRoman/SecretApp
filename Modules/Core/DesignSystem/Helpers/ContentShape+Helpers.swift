4c998cc65b9f3034f2865de4835c415e3f9679c8
Roman.Pro <136555465+RomanPronskiy@users.noreply.github.com>
2025-08-19 11:11:47 +0300

    Updated and extended color helpers

--- Modules/Core/DesignSystem/Helpers/ContentShape+Helpers.swift
+++ Modules/Core/DesignSystem/Helpers/ContentShape+Helpers.swift
@@ -2,12 +2,13 @@ import SwiftUI
 
 public extension ShapeStyle where Self == Color {
   // MARK: - Neutral
-  static var alphaNeutralAlpha16: Self { Color(hex: "#8D8D8D29") }
-  static var alphaNeutralAlpha54: Self { Color(hex: "#0002058A") }
+  static var alphaNeutralAlpha16: Self { Color(hex: "#8D8D8D").opacity(0.16) }
+  static var alphaNeutralAlpha54: Self { Color(hex: "#000205").opacity(0.54) }
 
   // MARK: - BG
   static var bgPrimary: Self { Color(hex: "#000205") }
   static var bgSecondary: Self { Color(hex: "#1A1A1A") }
+  static var bgTertiary: Self { Color(hex: "#000000").opacity(0.5) }
 
   // MARK: - Text
   static var textPrimary: Self { Color(hex: "#FFFFFF") }
@@ -16,5 +17,9 @@ public extension ShapeStyle where Self == Color {
   // MARK: - Button
   static var buttonPrimaryContentActive: Self { Color(hex: "#FFFFFF") }
   static var buttonPrimaryBgActive: Self { Color(hex: "#2979FF") }
-  static var buttonTertiaryBgActive: Self { Color(hex: "##FFFFFF00") }
+  static var buttonTertiaryBgActive: Self { Color(hex: "##FFFFFF").opacity(0) }
+
+  // MARK: - Gradient
+  static var gradientStart: Self { Color(hex: "#2979FF") }
+  static var gradientEnd: Self { Color(hex: "#29FFF4") }
 }
