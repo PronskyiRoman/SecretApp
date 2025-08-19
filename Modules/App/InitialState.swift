a8c64d893965ed45ddffacafb47e57dce6f82a71
Roman.Pro <136555465+RomanPronskiy@users.noreply.github.com>
2025-08-19 11:14:42 +0300

    Updated initial state of app

--- Modules/App/InitialState.swift
+++ Modules/App/InitialState.swift
@@ -1,5 +1,8 @@
-import MyCode
+import MyCodeUI
 
 extension SecretApp {
-  public static let initialState: SecretApp.State = .init(root: .init())
+  // since this screen should not be root screen of the App,
+  // but we want it to we are setting a mock implementation here
+  // created for UI
+  public static let initialState: SecretApp.State = .init(root: .init(myCode: .mock))
 }
