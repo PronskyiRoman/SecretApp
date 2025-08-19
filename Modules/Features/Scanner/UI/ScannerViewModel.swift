ff0cc525f6cbab87f32c67b8f4f7057203f9c179
Roman.Pro <136555465+RomanPronskiy@users.noreply.github.com>
2025-08-19 11:13:03 +0300

    Added ScannerViewModel

---
+++ Modules/Features/Scanner/UI/ScannerViewModel.swift
@@ -0,0 +1,10 @@
+import Foundation
+import DesignSystem
+
+struct ScannerViewModel {
+  let navigationTitle: String
+  let openGalleryButtonTitle: String
+  let openGalleryButtonIcon: Asset
+  let enableFlashlightButtonTitle: String
+  let enableFlashlightButtonIcon: Asset
+}
